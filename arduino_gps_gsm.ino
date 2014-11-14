fc286c2f3b5883a5526b910f65920d2f4da2637c
Parshin <ilyaiparshin@gmail.com>
2014-11-14 11:31:03 +0500

    a lot of optimization

--- arduino_gps_gsm.ino
+++ arduino_gps_gsm.ino
@@ -1,43 +1,33 @@
 //message format:
 //password#command*time
 //  3425#start*30
-//
 
 #include <MsTimer2.h>
-//#include <SimpleTimer.h>
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
 #define maxLength 256
 
 int8_t answer;
 
-String inData = String(maxLength);
 char data[maxLength];
 char phone_number[]="+79222620280";
 int x;
 int onModulePin = 2;
 int heater = 13;
-boolean logg = true;
-boolean first_loop = true;
 boolean nospd = false; // false = send data if speed > 5
-unsigned long prevMillis = 0;
-unsigned long currMillis;
+boolean hstarted = false; // heater started
 
 // GPS
-//bool newdata = false;
 unsigned long age, date, time, chars = 0, sat, hdopl;
 float flat, flon;
 int alt, cou, spd;
-//char c_lat[10], c_lon[10], c_cou[6], c_alt[6], c_spd[6];
 
 TinyGPS gps;
 
-//SimpleTimer timer;
+SoftwareSerial nss(9, 8); // GPS
+SoftwareSerial debug = SoftwareSerial(11, 10); // debug
 
-SoftwareSerial nss(9, 8);
-SoftwareSerial debug = SoftwareSerial(11, 10);
-
-//**************************************************************
+//*******************************************************************************************************************************
 void setup() {
   // Debug port
   debug.begin(9600);
@@ -47,66 +37,67 @@ void setup() {
   pinMode(onModulePin, OUTPUT);
   pinMode(heater, OUTPUT);
   Serial.begin(9600);
-  if (logg) {
-    debug.println("Starting...");
-  }
-  
+
+  debug.println("Starting");
+
+  delay(3000);
   power_on();
-  
   delay(3000);
 
-  if (logg) {
-    debug.println("Connecting to the GSM network...");
-  }
+  debug.println("Connecting to the GSM network...");
 
   // waiting GSM registration
-  while(sendATcommand2("AT+CREG?", "+CREG: 0,1", "+CREG: 0,5", 1000) == 0);
+  while(sendATcommand2("AT+CREG?", "+CREG: 0,1", "+CREG: 0,5", 30000) == 0);
   
   // Calling line identification presentation. 1: enable 
-  Serial.println("AT+CLIP=1");
-  delay(1000);
+  while(sendATcommand2("AT+CLIP=1", "OK", "OK", 1000) == 0);
   
   // Select SMS message format. 1: text mode
-  Serial.println("AT+CMGF=1");
-  delay(1000);
+  while(sendATcommand2("AT+CMGF=1", "OK", "OK", 1000) == 0);
   
   // New SMS message indication.
-  Serial.println("AT+CNMI=2,2,0,0,0");
-  delay(1000);
+  while(sendATcommand2("AT+CNMI=2,2,0,0,0", "OK", "OK", 1000) == 0);
 
-  if (logg) {
-    debug.println("Connected to the GSM network...");
-  }
-  
+  debug.println("Connected to the GSM network...");
+    
   // deliting SMS messages
   while(sendATcommand2("AT+CMGD=1,4", "OK", "OK", 2000) == 0);
 
   // GPS
-  if (logg) {
-    debug.println("Setting up GPS...");
-  }
+  debug.println("Setting up GPS...");
   nss.begin(4800);
-  if (logg) {
-    debug.println("Done setting up GPS!");
-  }  
   delay(200);
-
-  HeaterStop();
-   
-  InitGPRS();
+  debug.println("Done setting up GPS!");
   
-  if (logg) {
-    debug.println("Started!");
-  }  
+  debug.println("Setting up GPRS...");
+  InitGPRS();
+
+//  debug.println("Setting up timer...");
+//  delay(3000);
+//  MsTimer2::set(10000, SendGPSDataToServer);
+//  MsTimer2::start();
+//  delay(3000);
+  
+  debug.println("Done!");
+
 }
 
-//**************************************************************
+//*******************************************************************************************************************************
+void loop() {
+  getSerialChars();
+  checksms();
+  checkinccall();
+  delay(1000);
+  //t.update();
+}
+
+//*******************************************************************************************************************************
 void power_on(){
 
     uint8_t answer=0;
     
     // checks if the module is started
-    answer = sendATcommand2("AT", "OK", "OK", 2000);
+    answer = sendATcommand2("AT", "OK", "OK", 30000);
     if (answer == 0)
     {
         // power on pulse
@@ -122,7 +113,7 @@ void power_on(){
     
 }
 
-//**************************************************************
+//*******************************************************************************************************************************
 int8_t sendATcommand2(char* ATcommand, char* expected_answer1, char* expected_answer2, unsigned int timeout){
 
     uint8_t x=0,  answer=0;
@@ -134,13 +125,18 @@ int8_t sendATcommand2(char* ATcommand, char* expected_answer1, char* expected_an
     delay(100);
 
     while( Serial.available() > 0) Serial.read();    // Clean the input buffer
+    //getSerialChars();
 
     Serial.println(ATcommand);    // Send the AT command 
-
+    delay(100);
+    
     x = 0;
     previous = millis();
 
     // this loop waits for the answer
+    debug.print("Waiting for : ");
+    debug.println(ATcommand);
+    
     do{
         if(Serial.available() != 0){    // if there are data in the UART input buffer, reads it and checks for the asnwer
             response[x] = Serial.read();
@@ -156,154 +152,133 @@ int8_t sendATcommand2(char* ATcommand, char* expected_answer1, char* expected_an
         }
     }
     while((answer == 0) && ((millis() - previous) < timeout));    // Waits for the asnwer with time out
-
+    
+    debug.print("Response: ");
+    debug.println(response);
+    
     return answer;
 }
 
-//**************************************************************
+//*******************************************************************************************************************************
 void InitGPRS() { 
 
-  if (logg) {debug.println("AT&k3 - Hardware flow control activation.");}
-  // Flow control option. 3: Enable bi-directional hardware flow control.
-  Serial.println("AT&k3");
-  delay(1000);
-  getSerialChars();
+  debug.println("Setting Up hardware flow control activation...");
+  sendATcommand2("AT&k3", "OK", "OK", 2000);
 
   // GPRS Connection Configuration.
-  if (logg) {debug.println("AT+KCNXCFG - Setting GPRS paraameters (APN, login...).");}
+  debug.println("Setting Up GPRS parameters...");
   while(sendATcommand2("AT+KCNXCFG=0,\"GPRS\",\"internet\",\"\",\"\",\"0.0.0.0\",\"0.0.0.0\",\"0.0.0.0\"", "OK", "OK", 2000) == 0);
 
   // UDP Connection Configuration
-  if (logg) {debug.println("AT+KUDPCFG - Creating a new UDP socket."); }
-  while(sendATcommand2("AT+KUDPCFG=0,0", "OK", "OK", 4000) == 0);
+  //if (logg) {debug.println("AT+KUDPCFG - Creating a new UDP socket."); }
+  //while(sendATcommand2("AT+KUDPCFG=0,0", "OK", "OK", 4000) == 0);
   
-  if (logg) {debug.println("GPRS initiated!");}
+  debug.println("GPRS initiated!");
 
 }
 
-//**************************************************************
-void loop() {
-  getSerialChars();
-  // SMS
-  if (inData.indexOf('3425#') != -1) {
-    int IdxCmd = inData.indexOf('#');
-    int IdxTim = inData.indexOf('*');
-    String command = String(10);
-    String TimeStr = String(2);
-    command = inData.substring(IdxCmd + 1, IdxTim);
-    if (logg) {
-      debug.print("Cmd: ");
-      debug.println(command);
-    }
-    TimeStr = inData.substring(IdxTim + 1, inData.length());
-    char this_char[TimeStr.length() + 1];
-    TimeStr.toCharArray(this_char, sizeof(this_char));
-    long Time = atoi(this_char);
-    Time = Time * 1000 * 60;
-    
-    if (command == "start") {
+//*******************************************************************************************************************************
+void checksms(){
+  if (strstr(data,"3425#") != NULL) {
+    if (strstr(data,"3425#start") != NULL) {
       HeaterStart();
-      MsTimer2::set(Time, HeaterStop);
-      MsTimer2::start();
-      if (logg) {
-        debug.print("start on ");
-        debug.println(Time);
-      }
     }
-    if (command == "stop") {
+    if (strstr(data, "3425#stop") != NULL ) {
       HeaterStop();
     }
-    if (command == "getpos") {
-      GetGPSData(gps);
+    if (strstr(data, "3425#getpos") != NULL ) {
       sendSMSGPS();
     }
-    if (command == "int") {
-      SendGPSDataToServer(true);
+    if (strstr(data, "3425#int") != NULL ) {
+      SendGPSDataToServer();
     }
-    if (command == "nospd") {
+    if (strstr(data, "3425#nospd") != NULL ) {
       nospd = !nospd;
     }
+    if (strstr(data, "3425#status") != NULL ) {
+      HeaterStatus();
+    }
     
-    deleteAllMsgs();
-  }
-  // Incoming ring
-  if (inData.indexOf("+CLIP") != -1) {
-    int IdxCmd = inData.indexOf('+CLIP');
-    String incnum = inData.substring(IdxCmd + 4, IdxCmd + 16);
-    if (logg) {
-      debug.print("number: ");
-      debug.println(incnum);
-    }
-    if (incnum == "+79222620280") {
-      HeaterStart();
-      MsTimer2::set(1200000, HeaterStop);
-      MsTimer2::start();
-      //timer.setTimeout(1200000, HeaterStop);
-      if (logg) {debug.println("start by ring for 20 min.");}
-      delay(1000);
-      Serial.println("ATH"); // disconnect the call
-    }
-  }
-  //timer.run();
-  delay(1000);
-  currMillis = millis();
-  if (first_loop) {
-    prevMillis = 4000;
-    first_loop = false;
-  }
-  
-  if (currMillis - prevMillis > 60000) {
-    prevMillis = currMillis;
-    SendGPSDataToServer(nospd);
+    // deliting SMS messages
+    sendATcommand2("AT+CMGD=1,4", "OK", "OK", 60000);
   }
 }
 
-//*****************************************************************************************
+//*******************************************************************************************************************************
+void checkinccall(){
+  // Incoming ring
+  if (strstr(data, "+CLIP") != NULL) {
+    if (strstr(data, phone_number) != NULL) {
+      Heater();
+      sendATcommand2("ATH", "OK", "OK", 3000); // disconnect the call
+    }
+    else {
+      sendATcommand2("ATH", "OK", "OK", 3000); // disconnect the call
+    }
+  }
+
+}
+
+//**********************************************************************************************************************************************************
 void switchModule(){
   digitalWrite(onModulePin,HIGH);
   delay(2000);
   digitalWrite(onModulePin,LOW);
 }
 
-//**************************************************************
+//*******************************************************************************************************************************
+void Heater() {
+  
+  if (hstarted) {
+    HeaterStop();
+  }
+  else {
+    HeaterStart();
+  }
+  hstarted = !hstarted;
+}
+
+//*******************************************************************************************************************************
 void HeaterStart() {
   digitalWrite(heater, HIGH);
+  debug.println("Heater started.");
 }
 
-//**************************************************************
+//*******************************************************************************************************************************
 void HeaterStop() {
   digitalWrite(heater, LOW);
-  MsTimer2::stop();
-  if (logg) {debug.println("Heater stopped.");}
-
-//  Serial.print("AT+CMGS=\"");
-//  Serial.print(phone_number);
-//  Serial.println("\"");
-//  while(Serial.read()!='>');
-//  Serial.print("heater stopped");
-//  Serial.println("");
-//  delay(500);
-//  Serial.write(0x1A);       //sends ++
-//  Serial.write(0x0D);
-//  Serial.write(0x0A);
-
+  debug.println("Heater stopped.");
 }
 
-//**************************************************************
+//*******************************************************************************************************************************
+void HeaterStatus() {
+  
+  Serial.print("AT+CMGS=\"");
+  Serial.print(phone_number);
+  Serial.println("\"");
+  while(Serial.read()!='>');
+
+  Serial.print(hstarted);
+  Serial.println("");//
+
+  delay(500);
+  Serial.write(0x1A); //sends ++
+  Serial.write(0x0D);
+  Serial.write(0x0A); 
+}
+
+//*******************************************************************************************************************************
 void getSerialChars() {
-  for (x=0;x <= 255;x++){
-    data[x]='\0';                        
-  }
+  memset(data, '\0', sizeof(data));
   x=0;
-  while (Serial.available() > 0) {
+  while (Serial.available() != 0) {
     data[x] = Serial.read();
     x++;
   }
-  inData = data;
-  if (logg && inData.length() > 0) {debug.println(inData);}
+  if (data[0] != '\0') {debug.println(data);}
 }
 
-//**************************************************************
+//*******************************************************************************************************************************
 static bool feedgps(){
   while (nss.available())
   {
@@ -313,7 +288,7 @@ static bool feedgps(){
   return false;
 }
 
-//**************************************************************
+//*******************************************************************************************************************************
 void GetGPSData(TinyGPS &gps) {
     unsigned long start = millis();
     while (millis() - start < 1000)
@@ -328,20 +303,22 @@ void GetGPSData(TinyGPS &gps) {
     cou = gps.f_course();
     spd = gps.f_speed_kmph();
     
-    if (logg) {
-      debug.print("sat: "); debug.print(sat);
-      debug.print(" hdop: "); debug.print(hdopl);
-      debug.print(" lat: "); debug.print(flat, 6);
-      debug.print(" lon: "); debug.print(flon, 6);
-      debug.print(" alt: "); debug.print(alt);
-      debug.print(" cou: "); debug.print(cou);
-      debug.print(" spd: "); debug.print(spd);
-      debug.println();
-    }
+    debug.println("GPS data:");
+    debug.print("sat:"); debug.println(sat);
+    debug.print("hdop:"); debug.println(hdopl);
+    debug.print("lat:"); debug.println(flat, 6);
+    debug.print("lon:"); debug.println(flon, 6);
+    debug.print("alt:"); debug.println(alt);
+    debug.print("cou:"); debug.println(cou);
+    debug.print("spd:"); debug.println(spd);
+    debug.println();
 }
 
-//**************************************************************
+//*******************************************************************************************************************************
 void sendSMSGPS() {
+  
+  GetGPSData(gps);
+  
   Serial.print("AT+CMGS=\"");
   Serial.print(phone_number);
   Serial.println("\"");
@@ -374,22 +351,28 @@ void sendSMSGPS() {
   Serial.write(0x0A);
 }
 
-//**************************************************************
-void SendGPSDataToServer(boolean nospd) {
+//*******************************************************************************************************************************
+void SendGPSDataToServer() {
 
   GetGPSData(gps);
 
   if (spd < 5 && !nospd) {
-    if (logg) {debug.println("Data didn't send. Speed is less 5 km/h");} 
-    prevMillis = millis();
     return;
   }
   
-  if (logg) {debug.println("Connecting to server...");} 
+  debug.println("Creating a new UDP socket...");
+  if (sendATcommand2("AT+KUDPCFG=0,0", "OK", "OK", 60000) == 0) {
+     debug.println("Error creating UDP socket.");
+     return; 
+  }
 
-  while(sendATcommand2("AT+KUDPSND=1,\"87.254.138.72\",8186,70", "OK", "CONNECT", 4000) == 0);
+  debug.println("Connecting to server..."); 
+  //while(sendATcommand2("AT+KUDPSND=1,\"87.254.138.72\",8186,70", "OK", "CONNECT", 4000) == 0);
+  if (sendATcommand2("AT+KUDPSND=1,\"87.254.138.72\",8186,70", "CONNECT", "OK", 10000) == 0) {
+    return;
+  }
 
-  if (logg) {debug.println("sending data..."); }
+  debug.println("Sending data...");
   Serial.print("lat=");
   Serial.print(flat,6);
   Serial.print(";lon=");
@@ -404,28 +387,22 @@ void SendGPSDataToServer(boolean nospd) {
   Serial.write(10);
   Serial.write(13);
   Serial.print("--EOF--Pattern--");
-  delay(1000);
+  delay(100);
   Serial.println();
-  //getSerialChars();
+  getSerialChars();
 
 //  if (logg) {debug.println("AT+KUDPRCV...");}
 //  Serial.println("AT+KUDPRCV=1,70");
 //  delay(4000);
 //  getSerialChars();
 
-//  if (logg) {debug.println("close connections...");}
-//  Serial.println("AT+KUDPCLOSE=1");
-//  delay(1000);
-//  getSerialChars();
+  debug.println("Closing connections...");
+  while(sendATcommand2("AT+KUDPCLOSE=1", "OK", "OK", 4000) == 0);
+  
+  //getSerialChars();
 
-  prevMillis = millis();
-  if (logg) {debug.println("data sent.");}
+  //prevMillis = millis();
+  debug.println("data sent.");
 }
 
-//**************************************************************
-void deleteAllMsgs() {
-  delay(1000);
-  Serial.println("AT+CMGD=1,4");
-  delay(1000);
-}
-
+//*******************************************************************************************************************************
